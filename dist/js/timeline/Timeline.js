ad5def75a4fe76de472b05744da8ed5d70aa2c49
GPabis <grzegorz.pabis.96@gmail.com>
2020-10-15 03:36:35 +0200

    Implemented Timeline

--- dist/js/timeline/Timeline.js
+++ dist/js/timeline/Timeline.js
@@ -1,19 +1,60 @@
-class TimelineNav {
+class Timeline {
   constructor() {
+    this.timelineData = timelineData;
     this.currentCategory = "education";
     this.currentFirstItem = 0;
+    this._renderFirstTime();
   }
 
-  getCurrentCategory = () => this.currentCategory;
+  _getCurrentFirstItem = () => this.currentFirstItem;
 
-  setCurrentCategory = (category) => {
-    this.currentCategory = category;
+  _setCurrentFirstItem = (number) => {
+    this.currentFirstItem = number;
   };
 
-  getCurrentFirstItem = () => this.currentFirstItem;
+  _generateTimelineItems = () => {
+    const items = [];
+    this.timelineData[this.currentCategory].forEach((item, index) => {
+      items.push(
+        this._createTimelineItem(index, item.date, item.title, item.no)
+      );
+    });
+    document.querySelector(".timeline").innerHTML += items.join("");
+  };
 
-  setCurrentFirstItem = (number) => {
-    this.currentFirstItem = number;
+  _createTimelineItem = (itemIndex, date, title, itemNo) => {
+    return `
+    <li class="${this._setTimelineItemStyle(
+      itemIndex
+    )}" style="left: ${this._setTimelineAlign(itemIndex)}vw;">
+    <span class="timeline-date">${date}</span
+    ><span class="timeline-title"
+      >${title}</span
+    ><button class="timeline-more" data-index="${itemNo}">More...</button>
+  </li>`;
+  };
+
+  _resetTimelineStyles = () => {
+    const items = Array.from(document.querySelector(".timeline").children);
+    items.forEach((item) => (item.className = ""));
+  };
+
+  _setTimelineAlign = (itemIndex) => {
+    return 2 + 24 * itemIndex;
+  };
+
+  _setTimelineItemStyle = (itemIndex) => {
+    const itemPosition = itemIndex % 2 === 0 ? "item-top" : "item-bottom";
+    return itemPosition;
+  };
+
+  _setAgainTimelineStyles = () => {
+    const items = Array.from(document.querySelector(".timeline").children);
+    items.forEach((item, index) => {
+      item.style.cssText += `transform:translateX(${
+        24 * -this.currentFirstItem
+      }vw); transition-duration: .3s;`;
+    });
   };
 
   _setButtonActive = () => {
@@ -93,4 +134,23 @@ class TimelineNav {
     buttonLeft.addEventListener("click", this._prevItem);
     this._startEventListenerForCategory();
   };
+
+  _removeItems() {
+    document.querySelector(".timeline").innerHTML = "";
+  }
+
+  _renderFirstTime = () => {
+    if (this._removeItems()) _removeItems();
+    this._generateTimelineItems();
+    this._setButtonBlocked();
+    this._setButtonActive();
+    this._startEventListener();
+  };
+
+  _render = () => {
+    this._setAgainTimelineStyles();
+    this._setButtonBlocked();
+    this._setButtonActive();
+  };
 }
+const t = new Timeline();
---
+++ dist/js/timeline/TimelineData.js
@@ -0,0 +1,206 @@
+const timelineData = {
+  education: [
+    {
+      no: 0,
+      title: "High School - IT Technician",
+      date: "09.2012 - 04.2016",
+      pros: [
+        "Understanding the basics of the hardware",
+        "Esential knowledge of the IT software",
+        "Understanding how computer networks work",
+      ],
+      description:
+        "Technical school no. 1 in Andrychow, where for the first time I am started learnign things about software and hardware. For four years I gained knowledgo of computer networks, hardware exploatation, databases and Linux. I graduated from this school with the title of IT technician. ",
+    },
+    {
+      no: 1,
+      title:
+        "Silesian University of Technology - management and production engineering",
+      date: "10.2016 - 02.2018",
+      pros: [
+        "Knowledge of mathematics and mechanics",
+        "Knowledge of the industrial sector",
+        "Understanding of linear programming",
+      ],
+      description:
+        "First studies I was strarted after finishing high school. Becouse of lack of intrest in that faculie, I am never finished that studies. Despite this, I gained a lot of technical and economic knowledge. I got acquainted with the industrial sector, thanks to which I understood the important role of software in the operation of this sector of the economy.",
+    },
+    {
+      no: 2,
+      title:
+        "AGH University of Science and Technology - Computer Science and Econometrics",
+      date: "10.2018 - 06.2021",
+      pros: [
+        "Basics of C, C#, Python, R, Java, XML, SQL, Javascript",
+        "Strong knowledge of analitisc and statistics",
+        "Knowledge of management, IT industry and economy",
+      ],
+      description:
+        "Second studies, this time matches my interests and aspirations. I've gained here very broad knowledge about programming, thanks to this, I finded technology that fit to me. Also I lerned a lof about analitics and statistics.",
+    },
+    {
+      no: 3,
+      title: "Future Plans - Psychology Studies",
+      date: "??.???? - ??.????",
+      pros: [
+        "I will get psychology perspective on programming",
+        "I will get knowledge about people, that will improve my relations with coworkers",
+        "I will not have breake in learning",
+      ],
+      description:
+        "After finishing my bechlor or masters in Computer Science and Econometrics I will start next studies on psychology. Besides programming, I am very interested in social psychology. I think, that will helps me to get broad perspective about my work. Also after 40s I am planing to quit programming and start my own psychological office.",
+    },
+    {
+      no: 4,
+      title: "Future Plans - Psychology Studies",
+      date: "??.???? - ??.????",
+      pros: [
+        "I will get psychology perspective on programming",
+        "I will get knowledge about people, that will improve my relations with coworkers",
+        "I will not have breake in learning",
+      ],
+      description:
+        "After finishing my bechlor or masters in Computer Science and Econometrics I will start next studies on psychology. Besides programming, I am very interested in social psychology. I think, that will helps me to get broad perspective about my work. Also after 40s I am planing to quit programming and start my own psychological office.",
+    },
+    {
+      no: 5,
+      title: "Future Plans - Psychology Studies",
+      date: "??.???? - ??.????",
+      pros: [
+        "I will get psychology perspective on programming",
+        "I will get knowledge about people, that will improve my relations with coworkers",
+        "I will not have breake in learning",
+      ],
+      description:
+        "After finishing my bechlor or masters in Computer Science and Econometrics I will start next studies on psychology. Besides programming, I am very interested in social psychology. I think, that will helps me to get broad perspective about my work. Also after 40s I am planing to quit programming and start my own psychological office.",
+    },
+  ],
+  experience: [
+    {
+      no: 0,
+      title: "High School - IT Technician",
+      date: "09.2012 - 04.2016",
+      pros: [
+        "Understanding the basics of the hardware",
+        "Esential knowledge of the IT software",
+        "Understanding how computer networks work",
+      ],
+      description:
+        "Technical school no. 1 in Andrychow, where for the first time I am started learnign things about software and hardware. For four years I gained knowledgo of computer networks, hardware exploatation, databases and Linux. I graduated from this school with the title of IT technician. ",
+    },
+    {
+      no: 1,
+      title:
+        "Silesian University of Technology - management and production engineering",
+      date: "10.2016 - 02.2018",
+      pros: [
+        "Knowledge of mathematics and mechanics",
+        "Knowledge of the industrial sector",
+        "Understanding of linear programming",
+      ],
+      description:
+        "First studies I was strarted after finishing high school. Becouse of lack of intrest in that faculie, I am never finished that studies. Despite this, I gained a lot of technical and economic knowledge. I got acquainted with the industrial sector, thanks to which I understood the important role of software in the operation of this sector of the economy.",
+    },
+    {
+      no: 2,
+      title:
+        "AGH University of Science and Technology - Computer Science and Econometrics",
+      date: "10.2018 - 06.2021",
+      pros: [
+        "Basics of C, C#, Python, R, Java, XML, SQL, Javascript",
+        "Strong knowledge of analitisc and statistics",
+        "Knowledge of management, IT industry and economy",
+      ],
+      description:
+        "Second studies, this time matches my interests and aspirations. I've gained here very broad knowledge about programming, thanks to this, I finded technology that fit to me. Also I lerned a lof about analitics and statistics.",
+    },
+    {
+      no: 3,
+      title: "Future Plans - Psychology Studies",
+      date: "??.???? - ??.????",
+      pros: [
+        "I will get psychology perspective on programming",
+        "I will get knowledge about people, that will improve my relations with coworkers",
+        "I will not have breake in learning",
+      ],
+      description:
+        "After finishing my bechlor or masters in Computer Science and Econometrics I will start next studies on psychology. Besides programming, I am very interested in social psychology. I think, that will helps me to get broad perspective about my work. Also after 40s I am planing to quit programming and start my own psychological office.",
+    },
+    {
+      no: 4,
+      title: "FDDDDDDuture Plans - Psychology Studies",
+      date: "??.???? - ??.????",
+      pros: [
+        "I will get psychology perspective on programming",
+        "I will get knowledge about people, that will improve my relations with coworkers",
+        "I will not have breake in learning",
+      ],
+      description:
+        "After finishing my bechlor or masters in Computer Science and Econometrics I will start next studies on psychology. Besides programming, I am very interested in social psychology. I think, that will helps me to get broad perspective about my work. Also after 40s I am planing to quit programming and start my own psychological office.",
+    },
+  ],
+  volunteering: [
+    {
+      no: 0,
+      title: "DupaHigh School - IT Technician",
+      date: "09.2012 - 04.2016",
+      pros: [
+        "Understanding the basics of the hardware",
+        "Esential knowledge of the IT software",
+        "Understanding how computer networks work",
+      ],
+      description:
+        "Technical school no. 1 in Andrychow, where for the first time I am started learnign things about software and hardware. For four years I gained knowledgo of computer networks, hardware exploatation, databases and Linux. I graduated from this school with the title of IT technician. ",
+    },
+    {
+      no: 1,
+      title:
+        "Silesian University of Technology - management and production engineering",
+      date: "10.2016 - 02.2018",
+      pros: [
+        "Knowledge of mathematics and mechanics",
+        "Knowledge of the industrial sector",
+        "Understanding of linear programming",
+      ],
+      description:
+        "First studies I was strarted after finishing high school. Becouse of lack of intrest in that faculie, I am never finished that studies. Despite this, I gained a lot of technical and economic knowledge. I got acquainted with the industrial sector, thanks to which I understood the important role of software in the operation of this sector of the economy.",
+    },
+    {
+      no: 2,
+      title:
+        "AGH University of Science and Technology - Computer Science and Econometrics",
+      date: "10.2018 - 06.2021",
+      pros: [
+        "Basics of C, C#, Python, R, Java, XML, SQL, Javascript",
+        "Strong knowledge of analitisc and statistics",
+        "Knowledge of management, IT industry and economy",
+      ],
+      description:
+        "Second studies, this time matches my interests and aspirations. I've gained here very broad knowledge about programming, thanks to this, I finded technology that fit to me. Also I lerned a lof about analitics and statistics.",
+    },
+    {
+      no: 3,
+      title: "Future Plans - Psychology Studies",
+      date: "??.???? - ??.????",
+      pros: [
+        "I will get psychology perspective on programming",
+        "I will get knowledge about people, that will improve my relations with coworkers",
+        "I will not have breake in learning",
+      ],
+      description:
+        "After finishing my bechlor or masters in Computer Science and Econometrics I will start next studies on psychology. Besides programming, I am very interested in social psychology. I think, that will helps me to get broad perspective about my work. Also after 40s I am planing to quit programming and start my own psychological office.",
+    },
+    {
+      no: 4,
+      title: "Future Plans - Psychology Studies",
+      date: "??.???? - ??.????",
+      pros: [
+        "I will get psychology perspective on programming",
+        "I will get knowledge about people, that will improve my relations with coworkers",
+        "I will not have breake in learning",
+      ],
+      description:
+        "After finishing my bechlor or masters in Computer Science and Econometrics I will start next studies on psychology. Besides programming, I am very interested in social psychology. I think, that will helps me to get broad perspective about my work. Also after 40s I am planing to quit programming and start my own psychological office.",
+    },
+  ],
+};
